c16498021461742ed1dd24750016a2c39ffe489e
Alonso Parra <alonsogparra@gmail.com>
2019-03-26 11:37:20 -0400

    Shared.js built to open and close modal and mobile nav

---
+++ Module_09/shared.js
@@ -0,0 +1,46 @@
+var backdrop = document.querySelector('.backdrop');
+var modal = document.querySelector('.modal');
+var modalNoButton = document.querySelector('.modal__action--negative');
+var selectPlanButtons = document.querySelectorAll('.plan button');
+var toggleButton = document.querySelector('.toggle-button');
+var mobileNav = document.querySelector('.mobile-nav');
+
+for (var i = 0; i < selectPlanButtons.length; i++) {
+  selectPlanButtons[i].addEventListener('click', function () {
+    // modal.style.display = 'block';
+    // backdrop.style.display = 'block';
+    // modal.className = 'open'; // This will actually overwrite the complete class list
+    modal.classList.add('open');
+    backdrop.classList.add('open');
+  });
+}
+
+backdrop.addEventListener('click', function () {
+  // mobileNav.style.display = 'none';
+  mobileNav.classList.remove('open');
+  closeModal();
+});
+
+if (modalNoButton) {
+  modalNoButton.addEventListener('click', closeModal);
+}
+
+function closeModal () {
+  // modal.style.display = 'none';
+  // backdrop.style.display = 'none';
+  if (modal) {
+    modal.classList.remove('open');
+  }
+  backdrop.classList.remove('open');
+}
+
+toggleButton.addEventListener('click', function () {
+  // mobileNav.style.display = 'block';
+  // backdrop.style.display = 'block';
+  mobileNav.classList.add('open');
+  backdrop.classList.add('open');
+});
+
+// console.dir(backdrop);
+// console.dir(backdrop.style['background-image']);
+// console.dir(selectPlanButtons);
